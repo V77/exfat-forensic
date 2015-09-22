6bd190f8ec7aa9019dc0cdd2209a49c73247c6c7
ThomasMillochau <thomas.millochau@gmail.com>
2015-09-22 11:27:58 +0200

    Ajout commentaires VBR

--- fsstat.py
+++ fsstat.py
@@ -5,35 +5,38 @@ import struct
 import os
 
 
-
+#--------Fonction ffstat avec argument file1 (fichier dump à analyser)-------
 def fsstat(file1):
 
-#------------Ouverture Dump----------
+#--------Ouverture Dump----------
 	dump = file1
 	vbr_offset = 0x00006600
 	dump.seek(vbr_offset)
 	vbrcontent = dump.read(512)
 
-#-----------------Informations recerche dans VBR-------------------------
+#---------vbrcontent contient maintenant les 512 octets suivant l'offset de la vbr
+#---------nous pouvons donc travailler sur ce fichier pour recuperer les valeurs recherchées
+
+#---------Informations recerche dans VBR-------------------------
 # exFAT VBR
 #Taille de la VBR = 12 secteurs
-#0:2 Jmp Instruction
-#3:10 Signature exFAT
-#65:72 adresse VBR1 = sector number 0x33	debut disque = 0x33*512 = 0x6600
-#73:80 	Taille total du volume 
-#81:84	Addresse de la fat
-#85:88  taille de la fat
-#89:92  data cluster region adresse
-#93:96  number of cluster numer in the cluster heap
-#97:100 cluster adresse of root dir 
-#101:104 volume S/n
-#105:106 exfat version
-#107:108 flags
-#109 bytes per sector
-#110 sectors per cluster
-#end of bootcode
-#130 debut disque 
-
+#Signature emplacement [3:10]
+#Cluster Heap Offset emplacement [88:92]
+#Cluster Count emplacement [92:96]
+#Root dir first cluster emplacement [96:100]
+#partition offset emplacement [64:72]
+#taille volume emplacement [72:80]
+#Offset de la fat emplacement [80:84]
+#taille de la fat emplacement [84:88]
+#Volume serial number emplacement [100:104]
+#ytes per sector emplacement [108:109]
+#sectors per cluster emplacement [109:110]
+#nombre de fat emplacement [110:111]
+#drive select emplacement [111:112]
+#percent in use emplacement [112:113]
+#reserved emplacement [113:120]
+#bootcode emplacement [120:510]
+#bootsignature emplacement [510:512]
 
 
 #------------Affiche Resultats-----------------
@@ -42,12 +45,13 @@ def fsstat(file1):
 	signature = vbrcontent[3:10]
 	print "File System Type :",signature
 	print " "
-
+#---on recupere la signature de la partition situé au 3eme jusqu'au 10eme emplacement de notre variable vbrcontent
 
 	print "METADATA INFORMATION"
 	print "------------------------------------------------"
 	cluster_heap_offset = struct.unpack("<I", vbrcontent[88:92])[0]
 	print "Cluster Heap Offset : ", cluster_heap_offset
+#----on recupere l'offset du cluster et on le retourne (Little endian vers big endian) pour pouvoir l'afficher correctement
 
 	cluster_count = struct.unpack("<I", vbrcontent[92:96])[0]
 	print "Cluster Count : ",cluster_count
